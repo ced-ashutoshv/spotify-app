df7d9fabac5f5eeefb201bb0132ca9ab66d74664
Ashutosh Verma <ashutoshverma@cedcoss.com>
2022-05-12 14:34:56 +0530

    session completed

---
+++ src/app/controllers/RequestController.php
@@ -0,0 +1,9 @@
+<?php
+
+use Phalcon\Mvc\Controller;
+
+class RequestController extends Controller {
+    public function indexAction()
+    {
+    }
+}
---
+++ src/app/controllers/SessionController.php
@@ -0,0 +1,9 @@
+<?php
+
+use Phalcon\Mvc\Controller;
+
+class SessionController extends Controller {
+    public function indexAction()
+    {
+    }
+}
